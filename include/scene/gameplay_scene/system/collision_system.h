bb3a9b09187973c4e7a46f83ded5758caf866b3f
Ticketedmoon <shane.creedon3@gmail.com>
2023-10-08 16:02:02 +0100

    main: Manifold collision working
    Signed-off-by: Ticketedmoon <shane.creedon3@gmail.com>

--- include/scene/gameplay_scene/system/collision_system.h
+++ include/scene/gameplay_scene/system/collision_system.h
@@ -6,6 +6,9 @@
 #include <vector>
 #include <memory>
 
+#include <SFML/System/Vector3.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+
 #include "common_constants.h"
 #include "system.h"
 #include "entity/entity_manager.h"
@@ -25,7 +28,10 @@ class CollisionSystem : public System
 
         static void checkForWindowCollision(const std::shared_ptr<Entity>& e);
         static bool isCollidingAABB(const std::shared_ptr<CSprite>& renderComponentForEntity,
-                const std::shared_ptr<CSprite>& renderComponentForEnemy);
+                const std::shared_ptr<CSprite>& renderComponentForEnemy,
+                sf::FloatRect& overlap);
+        static sf::Vector3f getManifold(const sf::FloatRect& overlap, const sf::Vector2f& collisionNormal);
+        static void resolve(std::shared_ptr<CTransform>& cTransform, const sf::Vector3f& manifold);
 
     private:
         EntityManager& m_entityManager;
--- src/scene/gameplay_scene/system/collision_system.cpp
+++ src/scene/gameplay_scene/system/collision_system.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include "scene/gameplay_scene/system/collision_system.h"
 
 CollisionSystem::CollisionSystem(EntityManager& entityManager) : m_entityManager(entityManager)
@@ -24,10 +23,15 @@ void CollisionSystem::execute()
         for (const std::shared_ptr<Entity>& platform: platformsToUpdate)
         {
             std::shared_ptr<CSprite> playerSprite = std::static_pointer_cast<CSprite>(player->getComponentByType(Component::Type::SPRITE));
+            std::shared_ptr<CTransform> playerTransform = std::static_pointer_cast<CTransform>(player->getComponentByType(Component::Type::TRANSFORM));
             std::shared_ptr<CSprite> platformSprite = std::static_pointer_cast<CSprite>(platform->getComponentByType(Component::Type::SPRITE));
-            if (isCollidingAABB(playerSprite, platformSprite))
+
+            sf::FloatRect overlap;
+            if (isCollidingAABB(playerSprite, platformSprite, overlap))
             {
-                std::cout << "collision!" << '\n';
+                auto collisionNormal = platformSprite->m_shape.getPosition() - playerSprite->m_shape.getPosition();
+                auto manifold = getManifold(overlap, collisionNormal);
+                resolve(playerTransform, manifold);
             }
         }
     }
@@ -47,9 +51,38 @@ void CollisionSystem::checkForWindowCollision(const std::shared_ptr<Entity>& e)
     }
 }
 
-bool CollisionSystem::isCollidingAABB(const std::shared_ptr<CSprite>& renderComponentForEntity,
-        const std::shared_ptr<CSprite>& renderComponentForEnemy)
+bool CollisionSystem::isCollidingAABB(
+        const std::shared_ptr<CSprite>& renderComponentForEntity,
+        const std::shared_ptr<CSprite>& renderComponentForEnemy,
+        sf::FloatRect& overlap)
 {
     return renderComponentForEntity->m_shape.getGlobalBounds()
-        .intersects(renderComponentForEnemy->m_shape.getGlobalBounds());
+        .intersects(renderComponentForEnemy->m_shape.getGlobalBounds(), overlap);
+}
+
+sf::Vector3f CollisionSystem::getManifold(const sf::FloatRect& overlap, const sf::Vector2f& collisionNormal)
+{
+    // the collision normal is stored in x and y, with the penetration in z
+    sf::Vector3f manifold;
+
+    if (overlap.width < overlap.height)
+    {
+        manifold.x = (collisionNormal.x < 0) ? 1.f : -1.f;
+        manifold.z = overlap.width;
+    }
+    else
+    {
+        manifold.y = (collisionNormal.y < 0) ? 1.f : -1.f;
+        manifold.z = overlap.height;
+    }
+
+    return manifold;
+}
+
+void CollisionSystem::resolve(std::shared_ptr<CTransform>& cTransform, const sf::Vector3f& manifold)
+{
+    // move the shape out of the solid object by the penetration amount
+    sf::Vector2f normal(manifold.x, manifold.y);
+    const sf::Vector2<float>& offset = normal * manifold.z;
+    cTransform->m_position += offset;
 }
